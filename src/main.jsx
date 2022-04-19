1b171a262d3b2767e0f98130c5685edcb5129f9c
UnTalRenan <cardona_hs@hotmail.com>
2022-04-18 22:05:40 -0600

    make it better

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,13 @@ import App from './App'
 
 import {Auth0Provider} from '@auth0/auth0-react'
 ReactDOM.render(
+  
   <React.StrictMode>
+    
     <Auth0Provider domain='dev-vxx3w2jq.us.auth0.com' clientId='44VOxyfJNjVADBnV1QLIABVQaUhjAJyV' redirectUri={window.location.origin}>
       <App />
     </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
-/*import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)*/
